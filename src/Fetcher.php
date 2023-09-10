b9a3da1cfbaacf1a57893e0905d12190113dc4e4
Danilo Silva <danilocgsilva@gmail.com>
2023-09-09 22:00:43 -0300

    Added guzzle client

--- src/Fetcher.php
+++ src/Fetcher.php
@@ -2,10 +2,13 @@
 
 namespace Danilocgsilva\RepositoryHubFetcher;
 
+use GuzzleHttp\Client as HttpClient;
+
 class Fetcher
 {
     public function fetches()
     {
+        $httpClient = new HttpClient();
         return "hello world!";
     }
 }
